41a69b0a827f872fba9e33cb2d31cf2320201813
333736 <90184972+NMbo@users.noreply.github.com>
2022-10-05 13:36:19 +0200

    HRM demo create en update

---
+++ app/Views/Includes/navigation.php
@@ -0,0 +1,14 @@
+<nav class="top-nav">
+    <ul>
+        <li>
+            <a href="<?= URLROOT; ?>/index">Home</a>
+
+
+        </li>
+
+        <li>
+            <a href="<?= URLROOT; ?>/Applicant">Applicant</a>
+        </li>
+
+    </ul>
+</nav>
